84b9cdbed33025b05635235ef8828298e96b7063
adam harms <harms2a@gmail.com>
2022-12-24 11:07:07 -0600

    db add issue

---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+"""Sample Tests"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_number(self):
+        res = calc.add(5,6)
+        
+        self.assertEqual(res, 11)
+
+
+    def test_subtract_number(self):
+        res = calc.sub(7,6)
+        self.assertEqual(res, 1)
